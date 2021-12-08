ad64cc9e124077cf55b3b25f482da3ce6d026c0a
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-12-08 23:46:32 +0100

    feat: init

---
+++ components/Hello.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const props = defineProps({
+  count: {
+    type: Number,
+  }
+})
+
+const times = ref(2)
+
+const result = computed(() => props.count * times.value)
+</script>
+
+<template>
+  <pre>{{ count }} x {{ times }} = {{ result }}</pre>
+  <button @click="times += 1">More</button>
+</template>
